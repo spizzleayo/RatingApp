b5075ca23916e47c7f0b3f143c7d8b05dc64bf9b
spizzleayo <rajiayomikun@gmail.com>
2020-05-10 16:13:05 +0700

    set template engine ejs

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const engine = require('ejs-mate')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
 
 const app = express()
+// TODO connect to docker mongoDB
+// mongoose.connect('', { useMongoClient: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB at ')
+//     return mongoose.connection
+//   })
+//   .catch(err => console.log(`Database connection error: ${err.message}`))
 
 app.use(express.static('public'))
 app.engine('ejs', engine)
@@ -14,8 +23,20 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// session
+app.use(session({
+  secret: 'Thisismytestkey',
+  resave: false,
+  saveUninitialized: false
+  // store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/test', (req, res) => {
+  res.render('test')
 })
 
 app.listen(3000, () => {
